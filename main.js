f4bd60e1dc9798a1a6077dde788f7a53f532a40e
Adam Finch <leodafinchy@gmail.com>
2014-01-15 12:00:41 -0500

    Added primary layers

--- main.js
+++ main.js
@@ -8,6 +8,9 @@ var VectorApp = {
     width:800,
     height:450,
     stage:null,
+    layers:{},
+    backLayer:null,
+    frontLayer:null,
 };
 
 function DocReady(event)
@@ -17,4 +20,11 @@ function DocReady(event)
         width: VectorApp.width,
         height: VectorApp.height,
     });
+    VectorApp.backLayer = new Kinetic.Layer();
+    VectorApp.frontLayer = new Kinetic.Layer();
+    VectorApp.layers.Base = new Kinetic.Layer();
+    
+    VectorApp.stage.add(VectorApp.backLayer);
+    VectorApp.stage.add(VectorApp.layers.Base);
+    VectorApp.stage.add(VectorApp.frontLayer);
 }
