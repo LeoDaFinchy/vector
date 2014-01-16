434ab5f045cac676961adb6cce1056e1d3a11dfa
Adam Finch <leodafinchy@gmail.com>
2014-01-16 09:16:16 -0500

    Added cubic Interpolate

--- Bezier.js
+++ Bezier.js
@@ -29,3 +29,19 @@ Bezier.quadraticInterpolate = function(a, b, c, t)
     }
     return results;
 };
+Bezier.cubicInterpolate = function (a, b, c, d, t)
+{
+    var results = [];
+    for(var i in t){
+        var tp = t[i];
+        var tn = 1 - tp;
+        
+        var r0 = Math.pow(tn, 3) * a;
+        var r1 = 3 * Math.pow(tn, 2) * tp * b;
+        var r2 = 3 * tn * Math.pow(tp, 2) * c;
+        var r3 = Math.pow(tp, 3) * d;
+        
+        results.push(r0 + r1 + r2 + r3);
+    }
+    return results;
+};
--- test.js
+++ test.js
@@ -9,15 +9,16 @@ $('document').ready(test);
 function test(event)
 {
     var vec = [
-        new V(300, 100),
-        new V(450, 300),
-        new V(600, 100),
+        new V(300, 200),
+        new V(400, 300),
+        new V(500, 100),
+        new V(600, 200),
     ];
     
     var t = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0];
     
-    var x = Bezier.quadraticInterpolate(vec[0].x, vec[1].x, vec[2].x, t);
-    var y = Bezier.quadraticInterpolate(vec[0].y, vec[1].y, vec[2].y, t);
+    var x = Bezier.cubicInterpolate(vec[0].x, vec[1].x, vec[2].x, vec[3].x, t);
+    var y = Bezier.cubicInterpolate(vec[0].y, vec[1].y, vec[2].y, vec[3].y, t);
     
     console.log(x, y);
     
