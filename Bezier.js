f0b3d5552e5f5568b6f0aa003a5b515fe05de2d8
Adam Finch <leodafinchy@gmail.com>
2014-01-16 09:12:16 -0500

    Added quadratic interpolation

--- Bezier.js
+++ Bezier.js
@@ -14,3 +14,18 @@ Bezier.linearInterpolate = function(a, b, t)
     }
     return results;
 };
+Bezier.quadraticInterpolate = function(a, b, c, t)
+{
+    var results = [];
+    for(var i in t){
+        var tp = t[i];
+        var tn = 1 - tp;
+        
+        var r0 = Math.pow(tn, 2) * a;
+        var r1 = 2 * tn * tp * b;
+        var r2 = Math.pow(tp, 2) * c;
+        
+        results.push(r0 + r1 + r2);
+    }
+    return results;
+};
--- test.js
+++ test.js
@@ -10,13 +10,16 @@ function test(event)
 {
     var vec = [
         new V(300, 100),
+        new V(450, 300),
         new V(600, 100),
     ];
     
     var t = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0];
     
-    var x = Bezier.linearInterpolate(vec[0].x, vec[1].x, t);
-    var y = Bezier.linearInterpolate(vec[0].y, vec[1].y, t);
+    var x = Bezier.quadraticInterpolate(vec[0].x, vec[1].x, vec[2].x, t);
+    var y = Bezier.quadraticInterpolate(vec[0].y, vec[1].y, vec[2].y, t);
+    
+    console.log(x, y);
     
     for(var v in x)
     {
