f7ccbbd980a9102912356bc71064f689fd5ed7f7
Adam Finch <leodafinchy@gmail.com>
2014-01-16 10:16:55 -0500

    Added binomial coefficient function, fixed factorial to give correct answer for factorial(0)

--- MathUtil.js
+++ MathUtil.js
@@ -2,10 +2,14 @@ var MathUtil = {};
 
 MathUtil.factorial = function(n)
 {
-    var f = n;
-    for(var i = 2; i < n; i++)
+    var f = 1;
+    for(var i = 2; i <= n; i++)
     {
         f *= i;
     }
     return f;
+};
+MathUtil.binomialCoefficient = function(n, k)
+{
+    return MathUtil.factorial(n) / (MathUtil.factorial(k) * MathUtil.factorial(n - k));
 };
--- test.js
+++ test.js
@@ -22,11 +22,16 @@ function test(event)
     var y = Bezier.cubicInterpolate(vec[0].y, vec[1].y, vec[2].y, vec[3].y, t);
     
     console.log(x, y);
-    console.log(1, MathUtil.factorial(1));
-    console.log(2, MathUtil.factorial(2));
-    console.log(3, MathUtil.factorial(3));
-    console.log(4, MathUtil.factorial(4));
-    console.log(5, MathUtil.factorial(5));
+    
+    for(var i = 0; i <= 5; i++)
+    {
+        var s = "";
+        for(var j = 0; j <= i; j++)
+        {
+            s += MathUtil.binomialCoefficient(i, j) + ", ";
+        }
+        console.log(s);
+    }
     
     for(var v in x)
     {
