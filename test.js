227487cec08bfaeb28fe41b0c579ea2f16254618
LeoDaFinchy <leodafinchy@gmail.com>
2014-01-20 17:44:08 +0000

    Testing extrapolation

--- test.js
+++ test.js
@@ -3,6 +3,7 @@ var Bezier = Bezier || {};
 var Kinetic = Kinetic || {};
 var VectorApp = VectorApp || {};
 var MathUtil = MathUtil || {};
+var Nodule = Nodule || {};
 var V = Vector2;
 
 $('document').ready(test);
@@ -11,12 +12,16 @@ function test(event)
 {
     var vec = [
         new V(300, 200),
-        new V(400, 300),
-        new V(500, 100),
+        new V(400, 250),
+        new V(500, 150),
         new V(600, 200),
     ];
     
-    var t = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0];
+    var t = [];
+    for(var i = -2.0; i <= 3.0; i += 0.1)
+    {
+        t.push(i);
+    }
     
     var x = Bezier.cubicInterpolate(vec[0].x, vec[1].x, vec[2].x, vec[3].x, t);
     var y = Bezier.cubicInterpolate(vec[0].y, vec[1].y, vec[2].y, vec[3].y, t);
