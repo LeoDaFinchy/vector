0f59903834fa84ba49e327a8ff083fef4865d172
Adam Finch <leodafinchy@gmail.com>
2014-01-23 11:31:21 -0500

    Stroke now takes an initial list of Nodules

--- Stroke.js
+++ Stroke.js
@@ -1,9 +1,9 @@
 var Kinetic = Kinetic || {};
 
-function Stroke()
+function Stroke(nodules)
 {
     this.__proto__ = new Kinetic.Shape({
         fill: '#000000',
     });
-    this.nodules = [];
+    this.nodules = nodules;
 }
