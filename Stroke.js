e40cb4e5f064ce77216ee4cbb301dd30a3604dda
Adam Finch <leodafinchy@gmail.com>
2014-01-31 07:59:10 -0500

    Fixed curve drawing function, tabs

--- Stroke.js
+++ Stroke.js
@@ -1,21 +1,22 @@
 var Kinetic = Kinetic || {};
+var Vector2 = Vector2 || {};
 
 function Stroke(nodules)
 {
     Kinetic.Shape.call(this, {
-		fill: 'black',
-		stroke: 'black',
-	});
+        fill: 'black',
+        stroke: 'black',
+    });
     this.nodules = nodules;
-	this.sceneFunc(this.drawFunc);
-	this.draw = {
-		points: [],
-		commands: [],
-		commandsEnum: {
-			LINE: 0,
-			CURVE: 1,
-		},
-	};
+    this.sceneFunc(this.drawFunc);
+    this.draw = {
+        points: [],
+        commands: [],
+        commandsEnum: {
+            LINE: 0,
+            CURVE: 1,
+        },
+    };
     this.refreshDrawFunc();
 }
 Kinetic.Util.extend(Stroke, Kinetic.Circle);
@@ -26,53 +27,42 @@ Stroke.prototype.drawFunc = function(context)
     context.beginPath();
     context.moveTo(n[0].x(), n[0].y()); //temporary
     var p = 0;
-	var c = 0;
-	var pts = this.draw.points;
-	/*while(c < this.draw.commands.length)
-	{
-		if(this.draw.commands[c] = this.draw.commandsEnum.LINE)
-		{
-			context.lineTo(pts[p].x, pts[p].y);
-			p++;
-		}
-		else if(this.draw.commands[c] === this.draw.commands.CURVE)
-		{
-			context.bezierTo(pts[p].x, pts[p].y, pts[p + 1].x, pts[p + 1].y, pts[p + 2].x, pts[p + 2].y);
-			p += 3;
-		}
-	*/
-	/*
-	for(var nod = 1; nod < n.length; nod++)
+    var c = 0;
+    var pts = this.draw.points;
+    while(c < this.draw.commands.length)
     {
-        context.lineTo(n[nod].x(), n[nod].y());
+        if(this.draw.commands[c] == this.draw.commandsEnum.LINE)
+        {
+            context.lineTo(pts[p].x, pts[p].y);
+            p++;
+        }
+        else if(this.draw.commands[c] == this.draw.commandsEnum.CURVE)
+        {
+            context.bezierCurveTo(pts[p].x, pts[p].y, pts[p + 1].x, pts[p + 1].y, pts[p + 2].x, pts[p + 2].y);
+            p += 3;
+        }
+        c++;
     }
-    for(nod = n.length - 2; nod >= 0 ; nod--)
-    {
-        context.lineTo(n[nod].x(), n[nod].y());
-    }
-	*/
     context.closePath();
     context.fillStrokeShape(this);
 };
 
 Stroke.prototype.refreshDrawFunc = function()
 {
-	this.draw.points = [];
-	this.draw.commands = [
-		this.draw.commandsEnum.LINE,
-		this.draw.commandsEnum.CURVE,
-		this.draw.commandsEnum.LINE,
-		this.draw.commandsEnum.CURVE,
-	];
-	var n = this.nodules;
-	for(var nod = 0; nod < n.length; nod++)
+    this.draw.points = [];
+    this.draw.commands = [
+        this.draw.commandsEnum.LINE,
+        this.draw.commandsEnum.CURVE,
+        this.draw.commandsEnum.LINE,
+        this.draw.commandsEnum.CURVE,
+    ];
+    var n = this.nodules;
+    for(var nod = 0; nod < n.length; nod++)
     {
-        this.draw.points.push(new Vector2(n[nod].x, n[nod].y + 3));
+        this.draw.points.push(new Vector2(n[nod].x(), n[nod].y() + 3));
     }
     for(nod = n.length - 1; nod >= 0 ; nod--)
     {
-        this.draw.points.push(new Vector2(n[nod].x, n[nod].y - 3));
+        this.draw.points.push(new Vector2(n[nod].x(), n[nod].y() - 3));
     }
-	
-    //this.sceneFunc(this.drawFunc);
 };
