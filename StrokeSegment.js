2176248de22c10eb8e11cef7f6cb2edf8058f8b6
Adam Finch <leodafinchy@gmail.com>
2014-03-19 12:04:18 -0400

    Added Bezier curve drawing function to StrokeSegment

--- StrokeSegment.js
+++ StrokeSegment.js
@@ -1,6 +1,7 @@
 var Vector2 = Vector2 || {};
 var Stroke = Stroke || {};
 var StrokeNode = StrokeNode || {};
+var Bezier = Bezier || {};
 
 function StrokeSegment(stroke, a, b)
 {
@@ -28,7 +29,43 @@ StrokeSegment.prototype.addDrawCommands = function(points, commands, forward)
     if(this.type == this.typeEnum.LINE)
     {
         commands.push(Stroke.prototype.drawCommandsEnum.LINETO);
-        points.push(next.nodule.getPosition().add(Vector2.displacement(prev.nodule.getPosition(), next.nodule.getPosition()).normal().scale(next.nodule.radius())));
+        points.push(
+            next.nodule.getPosition()
+                .add(
+                    Vector2.displacement(prev.nodule.getPosition(), next.nodule.getPosition())
+                        .normal()
+                        .scale(next.nodule.radius())
+                    )
+            );
+    }
+    else if(this.type == this.typeEnum.CURVE)
+    {
+        commands.push(Stroke.prototype.drawCommandsEnum.BEZIERCURVETO);
+        
+        var anchorA = this.getApproachNode(prev);
+        var anchorB = this.getApproachNode(next);
+        
+        var third = 1.0/3.0;
+        var dX = Bezier.cubicDerivativeInterpolate(
+            prev.nodule.getPosition().x,
+            anchorA.nodule.getPosition().x,
+            anchorB.nodule.getPosition().x,
+            next.nodule.getPosition().x,
+            [third, third * 2, 1]
+            );
+        var dY = Bezier.cubicDerivativeInterpolate(
+            prev.nodule.getPosition().y,
+            anchorA.nodule.getPosition().y,
+            anchorB.nodule.getPosition().y,
+            next.nodule.getPosition().y,
+            [third, third * 2, 1]
+            );
+        var normalA = new Vector2(dX[0], dY[0]).normal().scale(anchorA.nodule.radius());
+        var normalB = new Vector2(dX[1], dY[1]).normal().scale(anchorB.nodule.radius());
+        var normalC = new Vector2(dX[2], dY[2]).normal().scale(next.nodule.radius());
+        points.push(anchorA.nodule.getPosition().add(normalA));
+        points.push(anchorB.nodule.getPosition().add(normalB));
+        points.push(next.nodule.getPosition().add(normalC));
     }
 };
 
